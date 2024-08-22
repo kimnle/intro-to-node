3861dc8f6f06118715969f4aa601b5aa8baf0928
Kim <72332310+kimnle@users.noreply.github.com>
2024-08-22 20:02:00 +1000

    feat: try catch implemented

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-const inquirer = require("inquirer");
-
 console.log("Dice roller app is now running!!");
 
 /**
@@ -17,15 +15,42 @@ function rollDice(diceSize = 20) {
 
 // console.log("Random dice roll result is: " + rollDice());
 
-const prompt = inquirer.createPromptModule();
+// const prompt = inquirer.createPromptModule();
 
-prompt ([
-    {
-        type: "number",
-        name: "dice_size",
-        message: "What size of dice do you wanna roll?"
+// try {
+//     prompt ([
+//         {
+//             type: "number",
+//             name: "dice_size",
+//             message: "What size of dice do you wanna roll?"
+//         }
+//     ]).then((answer) => {
+//         console.log(answer);
+//         console.log(rollDice(answer.dice_size));
+//     }).catch((error) => {
+//         console.log("Error from dot-catch block");
+//         console.log(error);
+//     })
+// } catch (error) {
+//     console.log("Reached the error catch block!!");
+//     console.log(error);
+// }
+
+const prompt = require("prompt-sync")({signit: true});
+
+try{
+    let userDiceSize = prompt("What size do you wanna roll?");
+
+    // check if user entered a number
+    if (Number.isNaN(userDiceSize)) {
+        throw new Error("User did not enter a number");
     }
-]).then((answer) => {
-    console.log(answer)
-    console.log(rollDice(answer.dice_size))
-});
+
+    // Check if number is valid (eg. 1 or greater, whole numbers)
+    let userInputAsNumber = parseInt(userDiceSize);
+
+    console.log(rollDice(userInputAsNumber));
+} catch (error) {
+    console.log("Error occured");
+    console.log(error);
+}
